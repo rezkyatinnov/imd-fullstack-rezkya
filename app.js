74d2ebf781e807e7d9c8888cb5974d44187d3c47
Mohamad Rezkya Feurstinnov Hairy <rezkyatinnov@gmail.com>
2017-09-15 03:02:55 +0800

    add db config and connection with mongoose

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config/config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// Connect to database
+mongoose.connect(config.database.local,{
+  useMongoClient: true,
+  /* other options */
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
