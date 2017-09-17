3c82a39c3b5cecc4ee296486460e4a11e9352333
Mohamad Rezkya Feurstinnov Hairy <rezkyatinnov@gmail.com>
2017-09-17 13:29:43 +0800

    add mutual friend endpoint functionality

--- routes/friends.js
+++ routes/friends.js
@@ -95,7 +95,7 @@ router.post('/add', function (req, res) {
 });
 
 
-// Register new friend
+// retrieve friend list
 router.post('/list', function (req, res) {
     if (req.body.email) {
         let userQuery = User.findOne({email: req.body.email}).exec();
@@ -140,6 +140,85 @@ router.post('/list', function (req, res) {
         });
     } else {
         resBuilder.buildBasic(res, false, 'missing "email" field');
+    }
+});
+
+// retrieve mutual friend list
+router.post('/mutual', function (req, res) {
+    if (req.body.friends &&
+        (req.body.friends.length == 2) &&
+        (req.body.friends[0].toUpperCase() != req.body.friends[1].toUpperCase())) {
+        let user1Query = User.findOne({email: req.body.friends[0]}).exec();
+        user1Query.then(function (user1) {
+            if(user1){
+
+                let user2Query = User.findOne({email: req.body.friends[1]}).exec();
+                user2Query.then(function (user2) {
+                    if (user2) {
+                        let friendQuery = Friend.find().select('users')
+                            .where('users').in([user1._id,user2._id]).exec();
+                        friendQuery.then(function (friendlist) {
+                            if(friendlist){
+                                var friendFunc = [];
+                                var friend1 = [];
+                                var friend2 = [];
+                                _.each(friendlist, function(friend) {
+                                    let friends = [friend.users[0].toString().toUpperCase(),friend.users[1].toString().toUpperCase()];
+                                    if(_.indexOf(friends,user1._id.toString().toUpperCase())>=0){
+                                        let i = _.indexOf(friends,user1._id.toString().toUpperCase())==0?1:0;
+                                        friend1.push(friend.users[i].toString());
+                                    }else if(_.indexOf(friends,user2._id.toString().toUpperCase()>=0)){
+                                        let i = _.indexOf(friends,user2._id.toString().toUpperCase())==0?1:0;
+                                        friend2.push(friend.users[i].toString());
+                                    }
+                                });
+                                var mutualfriends = _.intersection(friend1,friend2);
+                                console.log(friend1);
+                                console.log(friend2);
+                                console.log(mutualfriends);
+                                console.log(friendlist);
+                                _.each(mutualfriends,function (friend) {
+                                    friendFunc.push(function (callback) {
+                                        let userQuery = User.findOne({_id:friend}).select('email').exec();
+                                        userQuery.then(function (user) {
+                                            if(user){
+                                                callback(null,user.email);
+                                            }else{
+                                                callback(null,null);
+                                            }
+                                        })
+                                    });
+                                });
+                                async.parallel(
+                                    friendFunc,
+                                    function (err,results) {
+                                        if(results && results.length>0){
+                                            resBuilder.friendList(res,true,"retrieving mutual friends success",results);
+                                        }else{
+                                            resBuilder.friendList(res,true,"both users have no mutual friend",results);
+                                        }
+                                    }
+                                );
+                            }else{
+                                resBuilder.buildBasic(res, false, 'both users have no friend');
+                            }
+                        });
+                    }else{
+                        resBuilder.buildBasic(res, false, 'no user with email: '.concat(req.body.friends[1]));
+                    }
+                });
+            }else{
+                resBuilder.buildBasic(res, false, 'no user with email: '.concat(req.body.friends[0]));
+            }
+        });
+    } else if (!req.body.friends) {
+        resBuilder.buildBasic(res, false, 'missing "friends" field');
+    } else if (req.body.friends.length != 2) {
+        resBuilder.buildBasic(res, false, 'field "friends" should be an array of user emails with size of 2 items')
+    } else if (req.body.friends[0].toUpperCase() == req.body.friends[1].toUpperCase()) {
+        resBuilder.buildBasic(res, false, 'can\'t search mutual friends with same email')
+    } else {
+        resBuilder.buildBasic(res, false, 'unknown error')
     }
 });
 
